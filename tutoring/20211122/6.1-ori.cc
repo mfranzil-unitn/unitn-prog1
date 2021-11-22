37286ffdc9ca665c4d7266ae27705bc39f1cd5da
Matteo Franzil <matteofranzil@gmail.com>
2021-11-22 18:05:17 +0100

    BBBBBB

--- tutoring/20211122/6.1-ori.cc
+++ tutoring/20211122/6.1-ori.cc
@@ -4,25 +4,25 @@ using namespace std;
 // Inserire qui le DICHIARAZIONI delle funzioni
 
 void stampa_array(int *array, int dim) {
-  cout << "{ ";
-  for (int i = 0; i < dim; i++) {
-    cout << array[i] << " ";
-  }
-  cout << "}";
+    cout << "{ ";
+    for (int i = 0; i < dim; i++) {
+        cout << array[i] << " ";
+    }
+    cout << "}";
 }
 
 int main() {
-  int primo[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int secondo[10] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
-  int risultato[10];
+    int primo[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int secondo[10] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+    int risultato[10];
 
-  somma_incrociata(risultato, primo, secondo, 10);
+    somma_incrociata(risultato, primo, secondo, 10);
 
-  cout << "Le somme incrociate dei due array sono: ";
-  stampa_array(risultato, 10);
-  cout << endl;
+    cout << "Le somme incrociate dei due array sono: ";
+    stampa_array(risultato, 10);
+    cout << endl;
 
-  return 0;
+    return 0;
 }
 
 // Inserire qui le DEFINIZIONI delle funzioni
--- tutoring/20211122/6.2.cc
+++ tutoring/20211122/6.2.cc
@@ -2,47 +2,45 @@
 using namespace std;
 
 // Inserire qui le DICHIARAZIONI delle funzioni
-int* somma_incrociata(int* primo, int* secondo, int dim);
-void somma_incrociata_ric(int* risultato, int* primo, int* secondo, int start, int dim);
+int *somma_incrociata(int *primo, int *secondo, int dim);
+void somma_incrociata_ric(int *risultato, int *primo, int *secondo, int start, int dim);
 void stampa_array(int *array, int dim);
 
-
 int main() {
-  int primo[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int secondo[10] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+    int primo[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 11};
+    int secondo[11] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 66};
 
-  int* risultato = somma_incrociata(primo, secondo, 10);
+    int *risultato = somma_incrociata(primo, secondo, 11);
 
-  cout << "Le somme incrociate dei due array sono " << stampa_array(risultato, 10) << endl;
+    cout << "Le somme incrociate dei due array sono ";
+    stampa_array(risultato, 11);
+    cout << endl;
 
-  delete[] risultato;
+    delete[] risultato;
 
-  return 0;
+    return 0;
 }
 
 // Inserire qui le DEFINIZIONI delle funzioni
 
-int* somma_incrociata(int* primo, int* secondo, int dim)
-{
-  int *risultato = new int[dim];
-  prodotto_somma_incrociata_ric(risultato, primo, secondo, 0, dim);
-  return risultato
+int *somma_incrociata(int *primo, int *secondo, int dim) {
+    int *risultato = new int[dim];
+    somma_incrociata_ric(risultato, primo, secondo, 0, dim);
+    return risultato;
 }
 
 // Identica all'Esercizio 1
-void somma_incrociata_ric(int* risultato, int* primo, int* secondo, int start, int dim);
-{
-  if(dim > 0)
-  {
-    riusultato[start] = (primo[start] + secondo[dim-1]);
-    somma_incrociata_ric(risultato, primo, secondo, start+1, dim-1);
-  }
+void somma_incrociata_ric(int *risultato, int *primo, int *secondo, int start, int dim) {
+    if (dim > 0) {
+        risultato[start] = (primo[start] + secondo[dim - 1]);
+        somma_incrociata_ric(risultato, primo, secondo, start + 1, dim - 1);
+    }
 }
 
 void stampa_array(int *array, int dim) {
-  cout << "{";
-  for (int i = 0; i < dim; i++) {
-    cout << array[i] << " ";
-  }
-  cout << "}";
+    cout << "{ ";
+    for (int i = 0; i < dim; i++) {
+        cout << array[i] << " ";
+    }
+    cout << "}";
 }
