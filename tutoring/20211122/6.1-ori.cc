4433aec8fc01d18c01cbd2273bfc8cb6bf438e53
Matteo Franzil <matteofranzil@gmail.com>
2021-11-22 16:54:36 +0100

    modified

--- tutoring/20211122/6.1-ori.cc
+++ tutoring/20211122/6.1-ori.cc
@@ -3,11 +3,28 @@ using namespace std;
 
 // Inserire qui le DICHIARAZIONI delle funzioni
 
+void stampa_array(int *array, int dim) {
+  cout << "{ ";
+  for (int i = 0; i < dim; i++) {
+    cout << array[i] << " ";
+  }
+  cout << "}";
+}
+
 int main() {
-    int primo[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int secondo[10] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
-    cout << "La somma dei prodotti incrociati dei due array e’ " << somma_prodotto_incrociato(primo, secondo, 10) << endl;
-    return 0;
+  int primo[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int secondo[10] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+  int risultato[10];
+
+  somma_incrociata(risultato, primo, secondo, 10);
+
+  cout << "Le somme incrociate dei due array sono " << stampa_array(risultato, 10) << endl;
+
+  return 0;
 }
 
 // Inserire qui le DEFINIZIONI delle funzioni
+
+
+
+
